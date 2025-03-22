14163b0ae161ed4b0c8386f236e2069a362cdbe0
Eli Newlin <eli@wsnewlin.com>
2025-03-22 18:49:11 -0500

    duplicate nav bar removal

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,32 +1,25 @@
 import { Routes } from '@angular/router';
-import { LayoutComponent } from './components/layout/layout.component';
 
 export const routes: Routes = [
   {
     path: '',
-    component: LayoutComponent,
-    children: [
-      {
-        path: '',
-        redirectTo: 'tasks',
-        pathMatch: 'full'
-      },
-      {
-        path: 'tasks',
-        loadComponent: () => import('./features/tasks/pages/task-list/task-list.component').then(m => m.TaskListComponent)
-      },
-      {
-        path: 'tasks/new',
-        loadComponent: () => import('./features/tasks/pages/task-form/task-form.component').then(m => m.TaskFormComponent)
-      },
-      {
-        path: 'tasks/:id',
-        loadComponent: () => import('./features/tasks/pages/task-detail/task-detail.component').then(m => m.TaskDetailComponent)
-      },
-      {
-        path: 'tasks/:id/edit',
-        loadComponent: () => import('./features/tasks/pages/task-form/task-form.component').then(m => m.TaskFormComponent)
-      }
-    ]
+    redirectTo: 'tasks',
+    pathMatch: 'full'
+  },
+  {
+    path: 'tasks',
+    loadComponent: () => import('./features/tasks/pages/task-list/task-list.component').then(m => m.TaskListComponent)
+  },
+  {
+    path: 'tasks/new',
+    loadComponent: () => import('./features/tasks/pages/task-form/task-form.component').then(m => m.TaskFormComponent)
+  },
+  {
+    path: 'tasks/:id',
+    loadComponent: () => import('./features/tasks/pages/task-detail/task-detail.component').then(m => m.TaskDetailComponent)
+  },
+  {
+    path: 'tasks/:id/edit',
+    loadComponent: () => import('./features/tasks/pages/task-form/task-form.component').then(m => m.TaskFormComponent)
   }
 ];
